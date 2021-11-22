faf4ef2abc34e055941de5733879ab2ffe9c199a
Dave <bgm500844@gmail.com>
2021-11-22 10:55:33 -0800

    update

--- Snow Boarder 2/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder 2/Assets/Scripts/CrashDetector.cs
@@ -7,12 +7,14 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = .5f;
     [SerializeField] ParticleSystem Crashinbutts;
+    [SerializeField] AudioClip shippy2;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         { 
             Debug.Log("Bad Touch");
+            GetComponent<AudioSource>().PlayOneShot(shippy2);
             Invoke("ReloadScene", loadDelay);
             Crashinbutts.Play();
         }
--- Snow Boarder 2/Assets/Scripts/PlayerController.cs
+++ Snow Boarder 2/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = .2f;
     [SerializeField] float antiTorqueAmount = -0.2f;
+    [SerializeField] float boostSpeed = 50f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+    }
+
+    void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
