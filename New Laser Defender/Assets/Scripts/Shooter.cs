ada8b00dccd059b572c98456cabedde9739627f7
Dave <bgm500844@gmail.com>
2021-12-05 09:15:19 -0800

    update

--- New Laser Defender/Assets/Scripts/Shooter.cs
+++ New Laser Defender/Assets/Scripts/Shooter.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5;
-    [SerializeField] float firingRate = 0.2f;
+    [SerializeField] float baseFiringRate = 0.2f;
+    
+    [Header("AI")]
     [SerializeField] bool useAI;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minimumFiringRate = 0.1f;
 
-    public bool isFiring;
+    [HideInInspector] public bool isFiring;
 
     Coroutine firingCoroutine;
 
@@ -56,7 +61,13 @@ public class Shooter : MonoBehaviour
             }
 
             Destroy(instance, projectileLifetime);
-            yield return new WaitForSeconds(firingRate);
+
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance,
+                                                    baseFiringRate + firingRateVariance);
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+
+
+            yield return new WaitForSeconds(timeToNextProjectile);
         }
     }
 }
