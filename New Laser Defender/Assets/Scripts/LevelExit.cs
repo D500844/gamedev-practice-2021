482fefa8839d825765a615dccb8ba9584e4205ea
Dave <bgm500844@gmail.com>
2021-12-22 00:22:03 -0800

    update

---
+++ New Laser Defender/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+
+{
+    [SerializeField] float levelLoadWait = 1f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadVictoryLevel());
+    }
+
+    IEnumerator LoadVictoryLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadWait);
+        SceneManager.LoadScene("Victory");
+    }
+
+
+}
