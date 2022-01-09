beebd36a98dfe3c4a347f41c457cb4134bedbbbe
Dave <bgm500844@gmail.com>
2022-01-09 02:57:28 -0800

    fun tests
    very good

--- New Laser Defender/Assets/Scripts/BackgroundChange.cs
+++ New Laser Defender/Assets/Scripts/BackgroundChange.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-
-
 public class BackgroundChange : MonoBehaviour
 {
+    public SpriteRenderer spriteRend;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //trigger
         if (other.tag == "BGtag1")
         {
             Debug.Log("test2");
+            spriteRend.enabled = false;
+            Invoke("TurnItBackOn", 3f);
+            StartCoroutine(TurnItBackOff());
         }
+    }
+
+    public void TurnItBackOn()
+    {
+        spriteRend.enabled = true;
+    }
+
+    public IEnumerator TurnItBackOff()
+    {
+        yield return new WaitForSeconds(4f);
+        spriteRend.enabled = false;
     }
 }
 
