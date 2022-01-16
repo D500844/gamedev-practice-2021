859272e683bd0b96136d94381c2859b48b896821
Dave <bgm500844@gmail.com>
2022-01-16 13:58:50 -0800

    Power Up System
    Implemented and functional, no bugs.
    Prep for character select screen.
    Weapon speed up ability.
    Boss UI.
    Animations

--- New Laser Defender/Assets/Scripts/SpriteCollection.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SpriteCollection : MonoBehaviour
-{
-    public List<Sprite> Sprites;
-    public int rank;
-}
