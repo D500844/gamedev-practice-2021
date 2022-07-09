048cf0d0ce59dc94a47ca4744a5f0ff33c44c38b
Dave <bgm500844@gmail.com>
2022-07-08 17:51:49 -0700

    enemy movement success

--- tilelawlerzors/Assets/Scripts/EnemyMovement.cs
+++ tilelawlerzors/Assets/Scripts/EnemyMovement.cs
@@ -17,4 +17,16 @@ public class EnemyMovement : MonoBehaviour
     {
         myRigidbody.velocity = new Vector2 (moveSpeed, 0f);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+
 }
